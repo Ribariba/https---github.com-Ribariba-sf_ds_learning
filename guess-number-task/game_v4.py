4ddd9f615a56087746cb7844647f6f2b8a45737b
Ribariba <alex.bortnyakov@gmail.com>
2021-11-30 23:57:30 +0300

    Update game_v4.py

--- guess-number-task/game_v4.py
+++ guess-number-task/game_v4.py
@@ -4,21 +4,24 @@ import numpy as np
 
 number = np.random.randint(1, 101) # загадываем число
 
-count = 0 #счетчик попыток
+def random_predict(number):
+    count = 0 #счетчик попыток
+    max_number = 101 #исходные границы
+    min_number = 0
+    while True:
+        count += 1
+        predict_number = (max_number + min_number) // 2 #
+        if predict_number == number:
+            return count
+        elif predict_number > number: #перезаписываем границы диапозона, пока не отгадаем число
+            max_number = predict_number
+        elif predict_number < number:
+            min_number = predict_number
 
-max_number = 101  #исходные границы
-min_number = 0
+final_count = random_predict(number)
 
-#перезаписываем границы диапозона, пока не отгадаем число
+print(f'Отгадали число {number} за {final_count} попыток!')
 
-while True:
-    count += 1
-    predict_number = (max_number + min_number) // 2 
-    if predict_number == number:
-        print(f'Отгадали число {number} за {count} попыток!') 
-        break
-    elif predict_number > number:
-        max_number = predict_number
-    elif predict_number < number:
-        min_number = predict_number
-    
+if __name__ == "__main__":
+    # RUN
+    random_predict(number)
